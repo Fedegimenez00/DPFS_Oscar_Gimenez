9ec0a0a8e5b6e456847c76def30db05df44947ab
Valentino Parma <parmavalentinop@gmail.com>
2025-04-22 17:10:18 -0300

    sqlite3 added for development

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,30 @@
+const path = require('path')
 module.exports = {
     development: {
-      username: "root",
+    /*  username: "root",
       password: null,
       database: "zerotrust_db",
       host: "127.0.0.1",
-      dialect: "mysql",
+       dialect: "mysql", */
+      dialect: "sqlite", //Elemento temporal de desarrollo
+      storage: path.resolve(__dirname, '../../../databaseSQLite/zerotrust_db.sqlite')
     },
     test: {
-      username: "root",
+    /*  username: "root",
       password: null,
       database: "database_test",
       host: "127.0.0.1",
-      dialect: "mysql",
+      dialect: "mysql", */
+      dialect: "sqlite", //Elemento temporal de desarrollo
+      storage: path.resolve(__dirname, '../../../databaseSQLite/zerotrust_db.sqlite')
     },
     production: {
-      username: "root",
+    /*  username: "root",
       password: null,
       database: "database_production",
       host: "127.0.0.1",
-      dialect: "mysql",
+      dialect: "mysql", */
+      dialect: "sqlite", //Elemento temporal de desarrollo
+      storage: path.resolve(__dirname, '../../../databaseSQLite/zerotrust_db.sqlite')
     },
   };
