0c55c38be0d3c3625ddf933d4d1517f9a8053a0b
Fedegimenez00 <Fedegimenez00>
2025-04-29 16:04:08 -0300

    all seeders fixed

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -17,7 +17,20 @@ const loginValidator = [
     .withMessage('La contraseña debe tener al menos 5 caracteres'),];
 
 const registerValidator = [
+    check("name")
+    .notEmpty()
+    .withMessage("Debes ingresar un nombre"),
 
+    check("email")
+      .notEmpty()
+      .withMessage("Debes ingresar un email")
+      .bail()
+      .isEmail()
+      .withMessage("El dato ingresado no corresponde a un email"),
+
+    check("password")
+    .notEmpty()
+    .withMessage("Debes ingresar un password"),
 ]
 
 module.exports = { loginValidator, registerValidator }; 
