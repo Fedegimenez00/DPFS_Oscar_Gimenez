86062531853af3581d018d1abce395593a08f10a
Fedegimenez00 <federicogimenez99@hotmail.com>
2025-05-03 14:32:42 -0300

    AdminInterface & products catalog connected to sql database implemented

--- src/controllers/adminControllers.js
+++ src/controllers/adminControllers.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
 const {json} = require('express');
 const path = require('path');
+const db = require('../database/models');
 
 const adminController = {
-    index : (req, res) =>{
-        let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/products.json')));
-        res.render(path.resolve(__dirname, '../views/admin/adminInterface'), {products});
+    index : async (req, res) => {
+
+        /*let products = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../database/products.json')));
+        res.render(path.resolve(__dirname, '../views/admin/adminInterface'), {products});*/
+         try
+              { //Productos de la base de datos de SQL
+                const products = await db.Product.findAll({
+
+                  include: ["categories", "subcategories", "languages", 'users'] //Se incluye todo por referencia
+                
+                });
+              
+        
+              return res.render('admin/adminInterface', {products}); // Aquí enviamos "products" a la vista
+              } catch (error){
+                console.log(error);
+              }
 
         // return res.render('admin/adminInterface');
         
