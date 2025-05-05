6ebb6ae320f08c2328c683b33fa502f038aec918
Fedegimenez00 <federicogimenez99@hotmail.com>
2025-05-05 00:29:10 -0300

    minor fixes

--- public/js/previewImage.js
+++ public/js/previewImage.js
@@ -1,5 +1,6 @@
 const defaultImagePreview = '/imgs/Images/photo preview.webp';
-const imageFile = document.getElementById('courseimage, avatar');
+const imageFile = document.getElementById('image');
+//const imageFile2 = document.getElementById('avatar');
 //Agregar los elementos del ProfileEdit
 const imagePreview = document.getElementById('imgPreview')
 imageFile.addEventListener('change', e  =>{
@@ -14,3 +15,17 @@ imageFile.addEventListener('change', e  =>{
     };
     
 })
+/*
+imageFile2.addEventListener('change', e  =>{
+    if( e.target.files[0] ){
+        const reader = new FileReader( );
+        reader.onload = function( e ) {
+            imagePreview.src = e.target.result;
+        }
+        reader.readAsDataURL(e.target.files[0])
+    }else{
+        imagePreview.src = defaultImagePreview;
+    };
+    
+})
+*/
